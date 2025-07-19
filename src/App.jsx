247fdb0cfd45ab5f49e433057f9b67aada8a8543
imnotdeepak <dpkkolli@gmail.com>
2025-07-19 16:56:57 -0400

    Project Complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { gsap, ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails";
+import About from "./components/About";
+import Art from "./components/Art";
+import Menu from "./components/Menu";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -11,6 +14,9 @@ const App = () => {
       <Navbar />
       <Hero />
       <Cocktails />
+      <About />
+      <Art />
+      <Menu />
     </main>
   );
 };
